e56d58c184b71c0ea828b85e447b8a6a75ba995e
KNis031 <karl.j.simu@gmail.comkarl.j.simu@gmail.com>
2022-02-19 16:30:08 +0100

    Initial Commit

---
+++ WavWrite/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WavWrite
+//
+//  Created by Karl Simu on 2022-02-19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
